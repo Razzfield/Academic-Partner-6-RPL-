fdcf2f51fab47d951ab9b2f1a933bb9756162980
sulaiman666 <sulaimaniman6@gmail.com>
2018-05-28 01:06:00 +0700

    Find

---
+++ AP-BackEnd/find.php
@@ -0,0 +1,45 @@
+<?php
+    include 'config.php'
+
+    $user_id = $_GET['user_id']
+
+    //Input minat/dept
+    $partner = file_get_contents("php://input");
+    $request = json_decode($partner);
+
+    if($partner = "Departemen"){
+        $jurusan = $request->jurusan;
+        $jurusan0 = mysqli_query($connect,"SELECT jurusan FROM user_mahasiswa WHERE id = '$user_id' ");
+        
+        $query_teman=mysqli_query($connect,"SELECT * FROM user_mahasiswa where jurusan = '$jurusan0' "); 
+        while($result=mysqli_fetch_assoc($query_teman)){
+            $result_set[]=$result;
+            $data=array(
+                'data'=>$result,
+                'status'=>'200'
+            );
+        }  
+    }
+    else if($partner = "Minat"){
+        $minat = $request->minat;
+        $minat0 = mysqli_query($connect,"SELECT minat FROM user_mahasiswa WHERE id = '$user_id' ");
+        
+        $query_teman=mysqli_query($connect,"SELECT * FROM user_mahasiswa where minat = '$minat0' "); 
+        while($result=mysqli_fetch_assoc($query_teman)){
+            $result_set[]=$result;
+            $data=array(
+                'data'=>$result,
+                'status'=>'200'
+            );
+        }  
+    }
+    else{
+        $query_teman=mysqli_query($connect,"SELECT * FROM user_mahasiswa ");
+        while($result=mysqli_fetch_assoc($query_teman)){
+            $data=array(
+                'data'=>$result,
+                'status'=>'200'
+            )
+        }
+    }
+?>
