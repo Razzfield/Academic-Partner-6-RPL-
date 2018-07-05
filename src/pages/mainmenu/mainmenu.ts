ec5e93e89692eef9700065171375c8b303f5ad6d
mahinzevaa <mahinzevan@gmail.com>
2018-07-06 01:23:40 +0700

    Repush

--- src/pages/mainmenu/mainmenu.ts
+++ src/pages/mainmenu/mainmenu.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, PopoverController } from 'ionic-angular';
+import { NavController, NavParams, PopoverController, LoadingController, AlertController } from 'ionic-angular';
 import { ViewmemberPage } from '../viewmember/viewmember';
 import { FriendPopoverPage } from '../friend-popover/friend-popover';
 import { CreateTeamPage } from '../create-team/create-team';
@@ -10,12 +10,37 @@ import { CreateTeamPage } from '../create-team/create-team';
 })
 export class MainmenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public popoverCtrl: PopoverController,
+    public loadCtrl: LoadingController,
+    public alertCtrl: AlertController
+    //private data : Data, 
+    //public http : Http
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainmenuPage');
   }
+
+  /* getGroup(){
+    //apiGet
+      this.http.get(this.data.BASE_URL+"/todo_read.php?id_user="+this.id_user<--tolong sesuaikan will wkwk).subscribe(data => {
+      let response = data.json();
+      console.log(response);
+      if(response.status==200){
+        this.groups = response.data;
+        group = true;
+      }
+      else {
+        Alert("No Data");
+        group = false;
+      }
+    });
+    //apiGet  
+  } */
 
   lihat(){
     this.navCtrl.push(ViewmemberPage);
@@ -31,4 +56,3 @@ export class MainmenuPage {
     this.navCtrl.push(CreateTeamPage);
   }
 }
-
